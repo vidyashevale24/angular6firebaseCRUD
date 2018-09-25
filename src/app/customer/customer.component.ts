346052a79999e02c7521bf137b2cacd6b9ffd589
Vidya Shevale <vidya.shevale24@gmail.com>
2018-09-25 11:30:35 +0530

    modification in onSubmit function

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -15,21 +15,6 @@ export class CustomerComponent implements OnInit {
   formControls = this.customerService.form.controls;
   ngOnInit() {
   }
- /* onSubmit(){
-    this.submitted = true;
-    if(this.customerService.form.valid) {
-            if (this.customerService.form.get('$key').value == null)
-                this.customerService.insertCustomer(this.customerService.form.value);
-            else
-                this.customerService.updateCustomer(this.customerService.form.value);
-                this.showSuccessMessage=true;
-          setTimeout(() => this.showSuccessMessage =false ,1000)
-          this.submitted = false;
-          this.customerService.form.reset();
-
-    }
-  }*/
-
     onSubmit() {
         this.submitted = true;
         if (this.customerService.form.valid) {
